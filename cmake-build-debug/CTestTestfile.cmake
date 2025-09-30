e92e80c1f4398616dab9c857c82050a40e132735
marko-kucheriavyi <marko.kucheriavyi.shi.2023@lpnu.ua>
2025-09-30 18:27:31 +0200

    Remove all unnecessary files, keep only essential ones

--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Marko/CLionProjects/sorted-tree
-# Build directory: C:/Users/Marko/CLionProjects/sorted-tree/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(test_sorted "C:/Users/Marko/CLionProjects/sorted-tree/cmake-build-debug/test_sorted.exe")
-set_tests_properties(test_sorted PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/Marko/CLionProjects/sorted-tree/CMakeLists.txt;17;add_test;C:/Users/Marko/CLionProjects/sorted-tree/CMakeLists.txt;0;")
